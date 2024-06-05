f6ad73f316964972ac5c7b2c8c24247814c7e7eb
Prusselll <142349768+Prusselll@users.noreply.github.com>
2024-06-05 16:56:52 +0000

    <message about commit>

--- projects/platformer/platformer.js
+++ projects/platformer/platformer.js
@@ -43,8 +43,8 @@ $(function () {
     // Create platforms
     // You must decide the x position, y position, width, and height of the platforms
     // example usage: createPlatform(x,y,width,height)
-createPlatform(.             )
-
+createPlatform(100, 630, 100, 20)
+createPlatform(230,550,100,20)
 
     
     // TODO 2
